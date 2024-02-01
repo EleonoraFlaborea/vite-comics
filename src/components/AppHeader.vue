304b36cab0f84f3eeccc1075c4d955a73f8a4bc6
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-02-01 20:53:38 +0100

    add header structure and content

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,6 +4,58 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <!--HEADER-->
+    <header class="header-container">
+        <figure class="logo-section">
+            <img src="../assets/img/dc-logo.png" alt="dc-logo">
+        </figure>
+        <nav class="nav-section">
+            <ul>
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTIBLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a></li>
+            </ul>
+        </nav>
 
-<style></style>
+    </header>
+</template>
+
+<style>
+a {
+    text-decoration: none;
+    color: black;
+
+}
+
+ul {
+    list-style-type: none;
+}
+
+li {
+    display: inline;
+    padding: 0 10px;
+
+}
+
+/*header*/
+.header-container {
+    min-height: 60px;
+    padding: 20px 10px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+}
+
+.logo-section {
+    padding-right: 250px;
+}
+</style>
